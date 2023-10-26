fb60bd057b3afd64c339ef8515cef6490c2cac6d
zrno24 <damir.zrnic27@gmail.com>
2023-10-25 23:33:05 -0400

    Initial commit

---
+++ src/main/java/com/game/gameshopapp/GameShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.game.gameshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/gameshopapp/GameShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.gameshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
